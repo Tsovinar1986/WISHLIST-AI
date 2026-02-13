eacdf96a24764100660814be0d810e36f2569152
Tsovinar1986 <babakhanyan.tsovinar1986@gmail.com>
2026-02-13 14:33:12 +0400

    https://github.com/Tsovinar1986/WISHLIST-AI.git

--- Frontend/src/lib/supabase.ts
+++ Frontend/src/lib/supabase.ts
@@ -1,4 +1,5 @@
 // @ts-expect-error - Package will be installed during build
+
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
